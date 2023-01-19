22449fa5497b15da827012ebe3e4dc864e573c61
Eduardo Vitor Mokfa <82604894+Edumokfa@users.noreply.github.com>
2023-01-18 23:08:58 -0300

    Criada estrutura inicial do projeto

---
+++ setup/aulas/web/src/App.tsx
@@ -0,0 +1,16 @@
+//import { Habit } from './components/Habit'
+import { Cabecalho } from "./components/Cabecalho";
+import { SummaryTable } from "./components/SummaryTable";
+import "./index.css";
+
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Cabecalho />
+        <SummaryTable />
+      </div>
+    </div>
+  );
+}
